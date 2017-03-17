80897c6e69ec063c452944c9b59515be35e270c6
Forrest Cinelli <fccinelli@wpi.edu>
2017-03-17 19:13:28 -0400

    Do the hello world thing again

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+    protect_from_forgery with: :exception
+
+    def hello:
+        render html: "<b>Hello World!!</b>"
+    end
 end
